3c9e33ce3473589bbf85375e730f2f52c1489f8d
Unilion1221 <unilion1221@gmail.com>
2023-12-02 15:22:39 +0400

    commit 4

--- kiwi.js
+++ kiwi.js
@@ -11,6 +11,7 @@ const mDropDown = document.querySelectorAll(".drop-down");
 console.log(mDropDown);
 
 
+// FOR MOBILE
 for(let i = 0; i < mDropDown.length; i++){
     mDropDown[i].addEventListener("click",()=>{
         let dropoDownStatus = mDropDown[i].childNodes[3].style.display;
@@ -32,22 +33,45 @@ for(let i = 0; i < mDropDown.length; i++){
     })
 }
 
+// FOR PC
+for(let i = 0; i < mDropDown.length; i++){
+    mDropDown[i].addEventListener("click",()=>{
+        let dropoDownStatus = mDropDown[i].childNodes[3].style.display;
+
+        if(mDropDown[i].classList.contains("pc-drop-down") == true){
+            if(dropoDownStatus === "flex" ){
+                mDropDown[i].childNodes[3].style.display = "none";
+
+                mDropDown[i].childNodes[1].childNodes[1].classList.add("fa-caret-down");
+                mDropDown[i].childNodes[1].childNodes[1].classList.remove("fa-caret-up");
+            } else {
+                closeAllpc(mDropDown);
+                mDropDown[i].childNodes[3].style.display = "flex";
+
+                mDropDown[i].childNodes[1].childNodes[1].classList.remove("fa-caret-down");
+                mDropDown[i].childNodes[1].childNodes[1].classList.add("fa-caret-up");
+            }
+        }
+    })
+}
+
+
+
 function myFunction(x) {
     if (x.matches) { // If media query matches
       document.body.style.backgroundColor = "yellow";
       for(let i = 0; i < mDropDown.length; i++){
         mDropDown[i].classList.toggle("m-drop-down");
+        mDropDown[i].classList.remove("pc-drop-down");
       }
-
-      console.log(mDropDown[0].classList.contains("m-drop-down"));
+      closeAll(mDropDown);
     } else {
       document.body.style.backgroundColor = "pink";
       for(let i = 0; i < mDropDown.length; i++){
         mDropDown[i].classList.remove("m-drop-down");
+        mDropDown[i].classList.toggle("pc-drop-down");
       }
-      closeAll(mDropDown);
-
-      console.log(mDropDown[0].classList.contains("m-drop-down"));
+      closeAllpc(mDropDown);
     }
 }
   
@@ -61,17 +85,6 @@ myFunction(x);
 x.addEventListener("change", function() {
     myFunction(x);
 });
-
-
-// Closing other drop-downs
-
-function closeAll(arg){
-    for(let i = 0; i < arg.length; i++){
-        arg[i].childNodes[3].style.display = "none";
-        arg[i].childNodes[1].childNodes[1].classList.add("fa-caret-down");
-        arg[i].childNodes[1].childNodes[1].classList.remove("fa-caret-up");
-    }
-}
 
 // Close if pressed on any other place
 document.addEventListener("click", function(e){
@@ -91,5 +104,24 @@ for(let i = 0; i < underMenuOptions.length; i++){
 }
 
 
+// Closing other drop-downs mobile
 
+function closeAll(arg){
+    for(let i = 0; i < arg.length; i++){
+        if(arg[i].classList.contains("m-drop-down") == true){
+            arg[i].childNodes[3].style.display = "none";
+            arg[i].childNodes[1].childNodes[1].classList.add("fa-caret-down");
+            arg[i].childNodes[1].childNodes[1].classList.remove("fa-caret-up");
+        }
+    }
+}
 
+function closeAllpc(arg){
+    for(let i = 0; i < arg.length; i++){
+        if(arg[i].classList.contains("pc-drop-down") == true){
+            arg[i].childNodes[3].style.display = "none";
+            arg[i].childNodes[1].childNodes[1].classList.add("fa-caret-down");
+            arg[i].childNodes[1].childNodes[1].classList.remove("fa-caret-up");
+        }
+    }
+}
